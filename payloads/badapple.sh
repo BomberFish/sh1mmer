cc2569391a54c443c75b8cc703d4fc218ebf164d
BomberFish <bomberfish@bomberfish.ca>
2023-11-05 12:41:23 -0500

    add audio, should work

--- payloads/badapple.sh
+++ payloads/badapple.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 source /usr/local/payloads/lib/gui_lib.sh
 showimage /usr/share/sh1mmer-assets/Logs.png
-for file in /usr/local/payloads/bad_apple_img/*.png; do
+aplay /usr/local/payloads/badapple/badapple.wav &
+for file in /usr/local/payloads/badapple/*.png; do
 	showimage $file
 	sleep 0.03
 done
